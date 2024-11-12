087b5764a81ce50a6222dcd254abc481c56d2cae
erick_dias <133061667+erick-dias@users.noreply.github.com>
2024-11-12 20:15:52 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function somar() {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    const resultado = num1 + num2;
+    document.getElementById("resultado").innerText = "Resultado: " + resultado;
+}
+
+function subtrair() {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    const resultado = num1 - num2;
+    document.getElementById("resultado").innerText = "Resultado: " + resultado;
+}
